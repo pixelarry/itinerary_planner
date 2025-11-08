21bd2f361bffeea3de2d9644614830c1c336a50d
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2025-11-08 20:12:02 +0530

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pixelarry.itinerary_planner"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 10001
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
